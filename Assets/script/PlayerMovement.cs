ff9cf802bb8992bbf5292c7a8ec67973070b590f
Picardo-Sanchez <remi.prodeo@hotmail.com>
2018-12-14 15:09:31 +0100

    russe hour

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5.0f;
     public bool isOnGround = false;
     public float jumpPower = 7.0f;
+    public Weapon Weapon;
 
+    public Animator anim;
     private Transform _transform;
     private Rigidbody2D _rigidbody;
     private Direction playerDirection = Direction.RIGHT;
@@ -40,19 +42,73 @@ public class PlayerMovement : MonoBehaviour
     {
         float translate = Input.GetAxis("Horizontal") * speed;
         //transform.Translate(translate, 0, 0);
-        if (translate != 0)
+        ManageAnimations(translate);
+    }
+
+    private void ManageAnimations(float translate)
+    {
+        if (isOnGround)
         {
-            if (translate > 0)
+
+            if (translate != 0)
             {
-                playerDirection = Direction.RIGHT;
+                if (translate > 0)
+                {
+                    playerDirection = Direction.RIGHT;
+                    anim.Play("megakid right");
+
+                }
+                else if (translate < 0)
+                {
+                    playerDirection = Direction.LEFT;
+                    anim.Play("megakid left");
+
+                }
             }
-            else if (translate < 0)
+            else
             {
-                playerDirection = Direction.LEFT;
+                if (Weapon.Firing)
+                {
+                    switch (playerDirection)
+                    {
+                        case Direction.RIGHT:
+                            anim.Play("megakid tir fixe right");
+                            break;
+                        case Direction.LEFT:
+                            anim.Play("megakid tir fixe left");
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (playerDirection)
+                    {
+                        case Direction.RIGHT:
+                            anim.Play("idle right");
+                            break;
+                        case Direction.LEFT:
+                            anim.Play("idle left");
+                            break;
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            switch (playerDirection)
+            {
+                case Direction.LEFT:
+                    anim.Play("megakid jump left");
+                    break;
+                case Direction.RIGHT:
+                    anim.Play("megakid jump");
+                    break;
+
             }
         }
+        _rigidbody.velocity = new Vector2(translate, _rigidbody.velocity.y);
 
-        _rigidbody.velocity = new Vector2(translate, _rigidbody.velocity.y); 
     }
 
     void Jump()
